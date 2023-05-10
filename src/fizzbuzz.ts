48d294046cde3e1379c7c423fdeb740ec99d5d13
Javier González <jgestarriol@gmail.com>
2023-05-10 22:16:49 +0200

    Add test cases

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,11 +1,8 @@
-function fizzBuzz(start: number, end: number) {
+export function fizzBuzz(start: number, end: number) {
   let fizzBuzzList: any[] = [];
   for (let num = start; num <= end; num++) {
     fizzBuzzList.push(num);
-    if (num % 5 === 0 && num % 3 == 0) {
-      fizzBuzzList.pop();
-      fizzBuzzList.push("FizzBuzz");
-    }
+
     if (num % 5 === 0) {
       fizzBuzzList.pop();
       fizzBuzzList.push("Buzz");
@@ -14,7 +11,10 @@ function fizzBuzz(start: number, end: number) {
       fizzBuzzList.pop();
       fizzBuzzList.push("Fizz");
     }
+    if (num % 3 === 0 && num % 5 === 0) {
+      fizzBuzzList.pop();
+      fizzBuzzList.push("FizzBuzz");
+    }
   }
-  console.log(fizzBuzzList);
+  return fizzBuzzList;
 }
-fizzBuzz(1, 50);
